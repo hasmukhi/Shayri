73cceb70eb56a0a0291efd92e7b62b39017a6889
VNurtureTechnologies <hasmukhi>
2017-06-21 12:37:48 +0530

    last activity added

--- app/src/main/java/com/custom/edu/shayri/Quotes.java
+++ app/src/main/java/com/custom/edu/shayri/Quotes.java
@@ -1,6 +1,10 @@
 package com.custom.edu.shayri;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by VNurtureTechnologies on 21-06-2017.
@@ -8,4 +12,15 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Quotes extends AppCompatActivity {
 
+    TextView textViewQuote;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+        textViewQuote=(TextView)findViewById(R.id.quotetxt);
+        Intent intent = getIntent();
+        String quotes = intent.getStringExtra("quotePos");
+        textViewQuote.setText(quotes);
+
+    }
 }
